58a52d237119fde32a1ae7c9583307560ee7f2a3
duyquang6 <nguyenduyquang06@gmail.com>
2024-05-15 23:53:56 +0700

    feat: parse torrent file

--- build.zig
+++ build.zig
@@ -15,14 +15,14 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "zigtor",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    // const lib = b.addStaticLibrary(.{
+    //     .name = "zigtor",
+    //     // In this case the main source file is merely a path, however, in more
+    //     // complicated build scripts, this could be a generated file.
+    //     .root_source_file = b.path("src/root.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
 
     const bencode_module = b.addModule("zig-bencode", .{
         .root_source_file = b.path("zig-bencode/src/main.zig"),
@@ -33,11 +33,11 @@ pub fn build(b: *std.Build) void {
     // This declares intent for the library to be installed into the standard
     // location when the user invokes the "install" step (the default step when
     // running `zig build`).
-    b.installArtifact(lib);
+    // b.installArtifact(lib);
 
     const exe = b.addExecutable(.{
         .name = "zigtor",
-        .root_source_file = b.path("src/torrent_file.zig"),
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -74,11 +74,12 @@ pub fn build(b: *std.Build) void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/torrent_file.zig"),
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
+    lib_unit_tests.root_module.addImport("zig-bencode", bencode_module);
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
     // Similar to creating the run step earlier, this exposes a `test` step to
--- src/torrent_file.zig
+++ src/torrent_file.zig
@@ -2,62 +2,104 @@ const std = @import("std");
 const testing = std.testing;
 const print = std.debug.print;
 
-const bencode = @import("zig-bencode.zig");
-const TorrentFile = struct {
+const bencode = @import("bencode.zig");
+pub const TorrentFile = struct {
+    allocator: std.mem.Allocator,
     announce_list: [][]const u8 = undefined,
     comment: []const u8 = undefined,
     creation_date: u64 = undefined,
-    http_seeds: [][]const u8 = undefined,
-    info: TorrentInfo = undefined,
-};
-
-const TorrentInfo = struct {
     length: u64 = undefined,
     name: []const u8 = undefined,
-    piece_length: []const u8 = undefined,
+    info_hash: []const u8 = undefined,
+    piece_length: u64 = undefined,
     pieces: []const u8 = undefined,
+
+    fn deinit(self: *TorrentFile) void {
+        self.allocator.free(self.comment);
+        self.allocator.free(self.announce_list);
+        self.allocator.free(self.name);
+        self.allocator.free(self.info_hash);
+        self.allocator.free(self.pieces);
+    }
+
+    fn parse(allocator: std.mem.Allocator, file_path: []const u8) !TorrentFile {
+        const torrent_file = try std.fs.openFileAbsolute(file_path, .{});
+        defer torrent_file.close();
+        const file_content = try torrent_file.readToEndAlloc(allocator, std.math.maxInt(usize));
+        defer allocator.free(file_content);
+
+        var parsedTree = try bencode.ValueTree.parse(file_content, allocator);
+        defer parsedTree.deinit();
+        const root_map = parsedTree.root.Map;
+
+        const creation_date = bencode.mapLookup(root_map, "creation date").?.Integer;
+        const comment = bencode.mapLookup(root_map, "comment").?.String;
+        const announce_arr = bencode.mapLookup(root_map, "announce-list").?.Array;
+
+        var announce_urls = std.ArrayList([]const u8).init(allocator);
+        defer announce_urls.deinit();
+        for (announce_arr.items) |item| {
+            const urls = item.Array;
+            if (urls.items.len != 1) unreachable;
+
+            for (urls.items) |url| {
+                try announce_urls.append(url.String);
+            }
+        }
+
+        var comment_al = std.ArrayList(u8).init(allocator);
+        defer comment_al.deinit();
+        try comment_al.appendSlice(comment);
+
+        // Parse torrent info
+        const torrent_info_field = bencode.mapLookup(root_map, "info").?;
+        const torrent_info_map = torrent_info_field.Map;
+
+        const name = bencode.mapLookup(torrent_info_map, "name").?.String;
+        var name_al = std.ArrayList(u8).init(allocator);
+        defer name_al.deinit();
+        try name_al.appendSlice(name);
+
+        const length = bencode.mapLookup(torrent_info_map, "length").?.Integer;
+
+        const piece_length: u64 = @intCast(bencode.mapLookup(torrent_info_map, "piece length").?.Integer);
+
+        const pieces = bencode.mapLookup(torrent_info_map, "pieces").?.String;
+        var piece_al = std.ArrayList(u8).init(allocator);
+        defer piece_al.deinit();
+        try piece_al.appendSlice(pieces);
+
+        var info_bencoded = std.ArrayList(u8).init(allocator);
+        defer info_bencoded.deinit();
+        try torrent_info_field.stringifyValue(info_bencoded.writer());
+
+        print("torrent_file = {s}, file_size = {}, piece_length = {}, piece_hash_len = {}\n", .{ file_path, file_path.len, piece_length, pieces.len });
+
+        return TorrentFile{
+            .creation_date = @intCast(creation_date),
+            .comment = try comment_al.toOwnedSlice(),
+            .length = @intCast(length),
+            .name = try name_al.toOwnedSlice(),
+            .allocator = allocator,
+            .piece_length = piece_length,
+            .pieces = try piece_al.toOwnedSlice(),
+            .announce_list = try announce_urls.toOwnedSlice(),
+            .info_hash = try info_bencoded.toOwnedSlice(),
+        };
+    }
+
+    // fn buildTrackerURL(self: *TorrentFile, peer_id: [20]u8) void {}
 };
 
-pub fn parseTorrentFile(allocator: std.mem.Allocator, file_path: []const u8) !TorrentFile {
-    const torrent_file = try std.fs.openFileAbsolute(file_path, .{});
-    defer torrent_file.close();
-    const file_content = try torrent_file.readToEndAlloc(allocator, std.math.maxInt(usize));
-    defer allocator.free(file_content);
-
-    var parsedTree = try bencode.ValueTree.parse(file_content, allocator);
-    defer parsedTree.deinit();
-
-    print("torrent_file = {s}, file_size = {}\n", .{ file_path, file_path.len });
-    const creation_date = bencode.mapLookup(parsedTree.root.Map, "creation date").?.Integer;
-    const comment = bencode.mapLookup(parsedTree.root.Map, "comment").?.String;
-
-    var comment_owned = std.ArrayList(u8).init(allocator);
-    defer comment_owned.deinit();
-    try comment_owned.appendSlice(comment);
-
-    // Parse torrent info
-    const torrent_info_map = bencode.mapLookup(parsedTree.root.Map, "info").?.Map;
-    const name = bencode.mapLookup(torrent_info_map, "name").?.String;
-
-    var name_owned = std.ArrayList(u8).init(allocator);
-    defer name_owned.deinit();
-    try name_owned.appendSlice(name);
-
-    const length = bencode.mapLookup(torrent_info_map, "length").?.Integer;
-
-    const torrent_info: TorrentInfo = .{ .length = @as(i64, length) };
-
-    return TorrentFile{ .creation_date = @intCast(creation_date), .comment = try comment_owned.toOwnedSlice(), .info = torrent_info };
-}
 test "parse torrent file" {
-    const torrent_file = try parseTorrentFile(testing.allocator, "/home/ligt/zig/zigtor/zig-bencode/input/ubuntu-20.04-desktop-amd64.iso.torrent");
-    defer testing.allocator.free(torrent_file.comment);
+    var torrent_file = try TorrentFile.parse(testing.allocator, "/home/ligt/zig/zigtor/zig-bencode/input/ubuntu-20.04-desktop-amd64.iso.torrent");
+    defer torrent_file.deinit();
 
     try testing.expectEqual(1587649533, torrent_file.creation_date);
     try testing.expectEqualStrings(
         "Ubuntu CD releases.ubuntu.com",
         torrent_file.comment,
     );
+    try testing.expectEqualStrings("ubuntu-20.04-desktop-amd64.iso", torrent_file.name);
+    try testing.expectEqual(2, torrent_file.announce_list.len);
 }
-
-pub fn main() void {}
